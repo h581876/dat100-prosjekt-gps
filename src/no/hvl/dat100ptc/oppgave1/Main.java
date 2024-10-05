3bb2aba8aaf1a6a164a1d5404fb89d65796866bb
h581876 <581876@stud.hvl.no>
2024-10-05 13:56:08 +0200

    Oppgave 1 implementert

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		GPSPoint gpspoint = new GPSPoint (1,2.0,3.0,5.0);
+		
+		System.out.println(gpspoint.getTime());
+		
+		gpspoint.setTime(2);
+		
+		System.out.println(gpspoint);
 		
 	}
 
